88bc4c044f55bd9ae60743634acab23c590e0179
Surya Ghising <suryaghising2@gmail.com>
2021-12-06 16:38:18 +0545

    Flow basic

--- app/src/main/java/com/example/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/example/hiltpractice/MainActivity.kt
@@ -6,11 +6,28 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runBlocking {
+            getData().catch { e ->
+                Log.d("main", "onCreate: ${e.message}")
+            }.collect {
+                Log.d("main", "data is=== $it")
+            }
+        }
 
     }
+
+    private fun getData(): Flow<Int> = flow {
+        for (i in 1..5) {
+            kotlinx.coroutines.delay(1000)
+            emit(i)
+        }
+    }.flowOn(Dispatchers.IO)
 }
